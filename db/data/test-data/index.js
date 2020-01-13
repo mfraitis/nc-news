86534cde464e80777b05af09b2cdac7c46759f1b
Mohamed Fraitis <mohamedfraitis@Mohameds-MacBook-Air.local>
2020-01-13 09:39:08 +0000

    git ignore

---
+++ db/data/test-data/index.js
@@ -0,0 +1,6 @@
+const topicData = require("./topics");
+const articleData = require("./articles");
+const userData = require("./users");
+const commentData = require("./comments");
+
+exports.testData = { topicData, articleData, userData, commentData };
